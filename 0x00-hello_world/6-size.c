39d37d0e6ba183b9cb4356de9e6173df3ec58c81
root <lefentsemasete@gmail.com>
2022-09-08 12:40:22 -0700

    low level c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ *main - Prints is the sizeof var types
+ *
+ *Return:Always 0 (success)
+ */
+int main(Void)
+{ char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return(0);
+
+}
+
+
+
+
+
