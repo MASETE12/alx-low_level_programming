26a55a0653adc04407186470f7fe17505e5468df
root <lefentsemasete@gmail.com>
2022-09-08 16:59:35 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 /**
- *
  * main- Prints a text
+ *
  * Return: Always 1 (success)
  */
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the size of many var types
+ * main - print 
  *
  * Return: Always 0 (success)
  */
@@ -10,9 +10,9 @@ int b;
 long c;
 long long d;
 float e;
-{printf("Size of a char: %ld byte(s)\n", sizeof(a));
-printf("Size of an int: %ld byte(s)\n", sizeof(b));
-printf("Size of a long int: %ld byte(s)\n", sizeof(c));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-printf("Size of a float: %ld byte(s)\n", sizeof(e));
+{printf("Size of a char: %ld byte(s)\n", unsigned char sizeof(a));
+printf("Size of an int: %ld byte(s)\n", unsigned int sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", unsigned long int sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", unsigned long long int sizeof(d));
+printf("Size of a float: %ld byte(s)\n", unsigned float sizeof(e));
 return (0); }
