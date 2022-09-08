9b26a5a57dd57eb3c3ee49f2200cfc00b540618c
root <lefentsemasete@gmail.com>
2022-09-08 15:39:18 -0700

    low level c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
   *
   * main - print the string in the put function
@@ -9,9 +8,5 @@
   * Return: 0
   */
 int main(void)
-
-{
-  puts("\"Programming is like building a multilingual puzzle");
-
-			return (0); 
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  * main - print the string in the put function
  *
@@ -10,8 +7,5 @@
  * Return: 0
  */
 int main(void)
-{
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-}
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
