09ab1fd3e0d0ce578eb5d84ed7706245897bd6e7
MASETE12 <lefentsemasete@gmail.com>
2022-09-10 02:37:49 -0700

    9 print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,8 +6,9 @@
  */
 int main(void)
 { char c;
-for(c = 'a'; c <= 'z'; c++)
-{ if(c != 'e' && c != 'q')
+for (c = 'a'; c <= 'z'; c++)
+{ 
+if (c != 'e' && c != 'q')
 { putchar(c); } }
 putchar('\n');
 return (0); }
