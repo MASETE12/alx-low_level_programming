c95f2fba80e110af69390075605419894cdb1c56
MASETE12 <lefentsemasete@gmail.com>
2022-09-10 02:26:34 -0700

    9 print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,11 +6,9 @@
  */
 int main(void)
 { int c;
-for (c = '0'; c <= '9'; c++)
-
-{ 
-putchar(c);	
-if (c != '9')
+for(c = '0'; c <= '9'; c++)
+{ putchar(c);	
+if(c != '9')
 { putchar(',');
 putchar(' '); }	}
 putchar('\n');
