287209ac9c62929aa137cd95b321b53976750c2e
MASETE12 <lefentsemasete@gmail.com>
2022-09-10 01:26:56 -0700

    pri alph

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,15 +4,10 @@
  *
  * Return: Always (Success)
  */
-
 int main(void)
 { char c;
 for (c = 'a'; c <= 'z'; c++)
 { if (c != 'e' && c != 'q')
-{
-putchar(c);
-}
-}
+{ putchar(c); } }
 putchar('\n');
-return (0);
-}
+return (0); }
